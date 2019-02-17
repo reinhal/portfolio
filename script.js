30787b5c1c29bbfb71efcf54d025cfec486238c3
Lisa Reinhardt <reinhal@gmail.com>
2019-02-17 14:53:42 -0600

    updated highlighting

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function highlightAbout() {
     $("#nav-about").click(function() {
         setTimeout( function() {
-            $(".about-me-section").addClass("highlight")
-            $(".about-me-section").fadeIn(3000)
+            $(".aboutme-section").addClass("highlight")
         }, 1000);
         setTimeout( function() {
-            $(".about-me-section").removeClass("highlight")
-        }, 3000);
+            $(".aboutme-section").removeClass("highlight")
+        }, 2000);
     });
 }
 
@@ -14,35 +13,32 @@ function highlightProject() {
     $("#nav-project").click(function() {
         setTimeout( function() {
             $(".project-section").addClass("highlight")
-            $(".project-section").fadeIn(3000)
         }, 1000);
         setTimeout( function() {
             $(".project-section").removeClass("highlight")
-        }, 3000);
+        }, 2000);
     });
 }
 
 function highlightContact() {
     $("#nav-contact").click(function() {
         setTimeout( function() {
-            $(".contact-title").addClass("highlight-contact")
-            $(".contact-title").fadeIn(3000)
+            $("#contact").addClass("highlight-contact")
         }, 1000);
         setTimeout( function() {
-            $(".contact-title").removeClass("highlight-contact")
-        }, 3000);
+            $("#contact").removeClass("highlight-contact")
+        }, 2000);
     });
 }
 
 function highlightContactMe() {
     $("#nav-contact-me").click(function() {
         setTimeout( function() {
-            $(".contact-me").addClass("highlight")
-            $(".contact-me").fadeIn(3000)
+            $(".icon-responsive").addClass("highlight")
         }, 1000);
         setTimeout( function() {
-            $(".contact-me").removeClass("highlight")
-        }, 4000);
+            $(".icon-responsive").removeClass("highlight")
+        }, 2000);
     });
 }
 
